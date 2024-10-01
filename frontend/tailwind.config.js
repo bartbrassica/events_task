5769f02959af2fe088623b20fe92ffc88ac42786
bartos_k <b.kapusniak11@gmail.com>
2024-10-01 22:16:26 +0200

    feat: frontend init

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure you include the correct path for your React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
